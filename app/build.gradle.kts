10b954c67dd36904fdd2d5f1106b1db264192f5d
Alejandro <cordon.alejandro@gmail.com>
2025-01-02 18:34:55 +0100

    updated  version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.2.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
